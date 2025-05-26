4a1a058c118bceb9ce4b93cdb2fa27b7fa04d273
elouannh <elouannhosta27@gmail.com>
2025-05-26 11:39:55 +0200

    Pushing ex05

---
+++ ex05/include/harl_utils.hpp
@@ -0,0 +1,24 @@
+#ifndef HARL_UTILS_HPP
+# define HARL_UTILS_HPP
+
+# define BLACK		"\033[30m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define WHITE		"\033[37m"
+
+# define BBLACK		"\033[1;30m"
+# define BRED		"\033[1;31m"
+# define BGREEN		"\033[1;32m"
+# define BYELLOW	"\033[1;33m"
+# define BBLUE		"\033[1;34m"
+# define BMAGENTA	"\033[1;35m"
+# define BCYAN		"\033[1;36m"
+# define BWHITE		"\033[1;37m"
+
+# define RESET		"\033[0m"
+
+#endif
---
+++ ex05/src/Harl.cpp
@@ -0,0 +1,49 @@
+#include "harl_utils.hpp"
+#include "Harl.hpp"
+
+// Constructors & Destructors
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+// Methods
+void Harl::complain(std::string level) {
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void		(Harl::*handlers[4])() = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error,
+	};
+
+	for (int i = 0; i < 4; i++) {
+		if (level == levels[i])
+			return (this->*handlers[i])();
+	}
+
+	std::cerr << BRED "Unknown level of complaint." << std::endl;
+}
+
+// Private methods
+void Harl::debug() {
+	std::cout << BMAGENTA "I love having extra bacon for my "
+	"7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+	RESET << std::endl;
+}
+
+void Harl::info() {
+	std::cout << BBLUE "I cannot believe adding extra bacon costs more money. "
+	"You didn't put enough bacon in my burger! If you did, I wouldn't be asking"
+	" for more!" RESET << std::endl;
+}
+
+void Harl::warning() {
+	std::cout << BYELLOW "I think I deserve to have some extra bacon for free. "
+	"I've been coming for years whereas you started working here since last "
+	"month." RESET << std::endl;
+}
+
+void Harl::error() {
+	std::cout << BRED "This is unacceptable! "
+	"I want to speak to the manager now." RESET << std::endl;
+}
