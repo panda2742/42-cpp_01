f59167dfc40e62c7e1a186ce11cbe376aba7cd57
elouannh <elouannhosta27@gmail.com>
2025-05-28 15:40:56 +0200

    Added newline

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -5,11 +5,18 @@ int main() {
 
 	harl.complain("DEBUG");
 	std::cout << std::endl;
+
 	harl.complain("INFO");
 	std::cout << std::endl;
+
 	harl.complain("WARNING");
 	std::cout << std::endl;
+
 	harl.complain("ERROR");
 	std::cout << std::endl;
+
 	harl.complain("RANDOM`");
+	harl.complain("gewgwrg`");
+	harl.complain("");
+	harl.complain("hello world");
 }
